1281057cbbe41c3152a1742643e545d8fad7c3da
LaGregance <gregoire.taja@gmail.com>
2025-02-25 09:31:17 +0100

    Initial commit

---
+++ Example/mobilyflow-ios-sdk/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  mobilyflow-ios-sdk
+//
+//  Created by LaGregance on 02/25/2025.
+//  Copyright (c) 2025 LaGregance. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
