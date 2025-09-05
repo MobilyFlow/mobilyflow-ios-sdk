00ca954c899bc86e53388c69ab55e9ab9416b1e5
LaGregance <gregoire.taja@gmail.com>
2025-09-05 17:29:04 +0200

    Fix StorePrice

--- Sources/MobilyflowSDK/Utils/StorePrice.swift
+++ Sources/MobilyflowSDK/Utils/StorePrice.swift
@@ -11,23 +11,29 @@ class StorePrice {
     let priceMillis: Int
     let currency: String
     let regionCode: String
+    let platform: Platform?
 
-    init(priceMillis: Int, currency: String, regionCode: String) {
+    init(priceMillis: Int, currency: String, regionCode: String, platform: Platform?) {
         self.priceMillis = priceMillis
         self.currency = currency
         self.regionCode = regionCode
+        self.platform = platform
     }
 
     static func parse(_ storePrice: [String: Any]) -> StorePrice {
+        let platform = storePrice["platform"] as? String
         return StorePrice(
             priceMillis: storePrice["priceMillis"] as! Int,
             currency: storePrice["currency"] as! String,
-            regionCode: storePrice["regionCode"] as! String
+            regionCode: storePrice["regionCode"] as! String,
+            platform: platform == nil ? nil : Platform.parse(platform!)
         )
     }
 
     static func getDefaultPrice(_ storePrices: [[String: Any]], currentRegion: String?) -> StorePrice? {
-        let currentRegionPrice = storePrices.first { $0["regionCode"] as? String == currentRegion }
+        let currentRegionPrice = storePrices.first {
+            $0["regionCode"] as? String == currentRegion && ($0["platform"] as? String == nil || $0["platform"] as? String == "ios")
+        }
         if currentRegionPrice != nil {
             return StorePrice.parse(currentRegionPrice!)
         }
@@ -40,7 +46,9 @@ class StorePrice {
             return StorePrice.parse(defaultStorePrice!)
         }
 
-        let firstStorePrice = storePrices.first
+        let firstStorePrice = storePrices.first {
+            $0["platform"] as? String == nil || $0["platform"] as? String == "ios"
+        }
         if firstStorePrice != nil {
             return StorePrice.parse(firstStorePrice!)
         }
